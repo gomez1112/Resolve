2c45580f55143d22600b004c884ab6348b6e9d6f
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2022-05-22 22:25:22 -0400

    Initial Commit

---
+++ Resolve/ResolveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolveApp.swift
+//  Resolve
+//
+//  Created by Gerard Gomez on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ResolveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
